6a41b468b93ceb059c2177dfb0ddd1baceed25b3
keke <dondohare55@gmail.com>
2022-02-27 19:22:44 +0900

    middleware

---
+++ api/middlewares/token_auth_middleware.go
@@ -0,0 +1,20 @@
+package middlewares
+
+import (
+	"know-sync-api/utils/auth_utils"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TokenAuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := auth_utils.TokenValid(c.Request)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
