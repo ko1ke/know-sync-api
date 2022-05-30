ff56aa7f086bedde3b62679bcc339f4baa3f65ea
keke <dondohare55@gmail.com>
2022-05-30 19:01:49 +0900

    add redis and var

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,12 +18,18 @@ resource "heroku_app" "app" {
   # (Optional) not necessary if you are only using assets precompile.used only assets:precompile
   buildpacks = ["heroku/go"]
   # (Optional) enviroment variables.
-  # config_vars = {
-  #   DATABASE_URL = var.db_url
-  # }
+  config_vars = {
+    ACCESS_SECRET  = var.access_secret
+    REFRESH_SECRET = var.refresh_secret
+  }
 }
 
 resource "heroku_addon" "database" {
   app_id = var.app-id
   plan   = "heroku-postgresql:hobby-dev"
+}
+
+resource "heroku_addon" "redis" {
+  app_id = var.app-id
+  plan   = "heroku-redis:hobby-dev"
 }
